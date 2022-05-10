138ea3dd6c076e90de0adbbaa46c2308b2b46298
Albin Dahlén <b21albda@student.his.se>
2022-05-10 11:22:47 +0200

    Added fragments, JsonTask class and implemented navigation between fragments

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.btm_nav);
+        NavController navController = Navigation.findNavController(this, R.id.host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
     }
 }
