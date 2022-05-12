5b0b059df1b12b38eb8de5d2c4387f2b06d65000
Albin Dahlén <b21albda@student.his.se>
2022-05-12 10:25:08 +0200

    Added abouticon in toolbar and setup intent to AboutActivity

--- app/src/main/java/com/example/foodapp/AboutActivity.java
+++ app/src/main/java/com/example/foodapp/AboutActivity.java
@@ -10,5 +10,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setTitle("About");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,6 +27,21 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         NavController navController = Navigation.findNavController(this, R.id.host_fragment);
 
         NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(this, AboutActivity.class);
+
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
