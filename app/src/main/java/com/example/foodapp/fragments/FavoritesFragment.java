efbcb3816834ef869e5f6bf709971a45c8cd0219
Albin Dahlén <b21albda@student.his.se>
2022-05-15 15:22:07 +0200

    Added recyclerview implementation and message to display when favorites is empty

--- app/src/main/java/com/example/foodapp/fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/foodapp/fragments/FavoritesFragment.java
@@ -1,66 +1,90 @@
 package com.example.foodapp.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.foodapp.DetailActivity;
 import com.example.foodapp.R;
+import com.example.foodapp.SelectListener;
+import com.example.foodapp.adapters.FoodAdapter;
+import com.example.foodapp.data.Food;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FavoritesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FavoritesFragment extends Fragment {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class FavoritesFragment extends Fragment implements SelectListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private FoodAdapter adapter;
+    private List<Food> favorites = new ArrayList<>();
+    private TextView tv_info;
 
-    public FavoritesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoritesFragment newInstance(String param1, String param2) {
-        FavoritesFragment fragment = new FavoritesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private SharedPreferences ref;
+    private SharedPreferences.Editor editor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        ref = this.getActivity().getSharedPreferences("Favorites", MODE_PRIVATE);
+        editor = ref.edit();
+
+        tv_info = view.findViewById(R.id.tv_info);
+
+        String list = ref.getString("FavoritesList", null);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Food>>() {}.getType();
+        favorites = gson.fromJson(list, type);
+
+
+        recyclerView = view.findViewById(R.id.favorites_recyclerView);
+        adapter = new FoodAdapter(favorites, this::onItemClicked);
+        adapter.setFoods(favorites);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        if (adapter.getItemCount() == 0)
+            tv_info.setText("No favorites yet");
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        String list = ref.getString("FavoritesList", null);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Food>>() {}.getType();
+        favorites = gson.fromJson(list, type);
+        adapter.setFoods(favorites);
+        adapter.notifyDataSetChanged();
+
+        if (adapter.getItemCount() == 0)
+            tv_info.setText("No favorites yet");
+
+        super.onResume();
+    }
+
+    @Override
+    public void onItemClicked(Food food) {
+        Intent intent = new Intent(this.getContext(), DetailActivity.class);
+        intent.putExtra("food", food);
+        startActivity(intent);
     }
 }
