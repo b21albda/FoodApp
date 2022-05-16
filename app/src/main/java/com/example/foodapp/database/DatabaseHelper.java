099787ec05d7e20e1c7d8c70407a1a9ce24d0788
Albin Dahlén <b21albda@student.his.se>
2022-05-16 12:26:29 +0200

    Added insert method in helper

--- app/src/main/java/com/example/foodapp/database/DatabaseHelper.java
+++ app/src/main/java/com/example/foodapp/database/DatabaseHelper.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.foodapp.data.Auxdata;
 import com.example.foodapp.data.Food;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -26,5 +27,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL(DatabaseTables.DELETE_TABLE_FAVORITES);
         onCreate(sqLiteDatabase);
+    }
+
+    /**
+     * Insert a food object to the db
+     *
+     * @param food The object
+     */
+    public void insert(Food food) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        Auxdata auxdata = food.getAuxdata();
+
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_NAME, food.getName());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_SIZE, food.getSize());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_COST, food.getCost());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_CATEGORY, food.getCategory());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_DESCRIPTION, auxdata.getDescription());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_IMG, auxdata.getImg());
+        values.put(DatabaseTables.FAVORITES.COLUMN_NAME_TYPE, auxdata.getType());
+
+        db.insert(DatabaseTables.FAVORITES.TABLE_NAME, null, values);
     }
 }
