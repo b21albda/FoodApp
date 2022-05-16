ed998e987e189c3d6887e03cd58c5760fbf22e14
Albin Dahlén <b21albda@student.his.se>
2022-05-16 12:21:03 +0200

    Added DatabaseHelper and DatabaseTables

---
+++ app/src/main/java/com/example/foodapp/database/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.foodapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.foodapp.data.Food;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "Favorites.db";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DatabaseTables.CREATE_TABLE_FAVORITES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DatabaseTables.DELETE_TABLE_FAVORITES);
+        onCreate(sqLiteDatabase);
+    }
+}
