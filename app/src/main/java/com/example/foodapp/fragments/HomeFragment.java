d0ef8d29d8e83db42c5d71e152aedd0f8803264c
Albin Dahlén <b21albda@student.his.se>
2022-05-17 15:54:24 +0200

    Added sort menu and added onclicklistener

--- app/src/main/java/com/example/foodapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/foodapp/fragments/HomeFragment.java
@@ -7,9 +7,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import com.example.foodapp.activities.DetailActivity;
 import com.example.foodapp.JsonTask;
@@ -35,14 +40,28 @@ public class HomeFragment extends Fragment implements JsonTask.JsonTaskListener,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        AutoCompleteTextView sortBox = view.findViewById(R.id.sort_box);
+        String[] t = {"Sort by", "Beverage", "Food", "Wine", "Beer", "Meat"};
+        ArrayAdapter test = new ArrayAdapter(this.getContext(), R.layout.dropdown_item, t);
+        sortBox.setAdapter(test);
+        sortBox.setOnItemClickListener((adapterView, view1, i, l) -> {
+            TextView tv = view1.findViewById(R.id.tv_option);
+            sortBy(tv.getText().toString());
+        });
+
         recyclerView = view.findViewById(R.id.home_recyclerView);
-        adapter = new FoodAdapter(foods, this::onItemClicked);
+        adapter = new FoodAdapter(foods, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=b21albda");
         // Inflate the layout for this fragment
         return view;
+    }
+
+    private void sortBy(String sortBy) {
+        Log.d("TAG", "sortBy: " + sortBy);
+
     }
 
     @Override
